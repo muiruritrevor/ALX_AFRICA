4cb1ead594b2d14c7ffffb8f4bdf104840754061
muiruritrevor <trevormuirurii@gmail.com>
2024-08-10 01:46:52 +0300

    commit

--- Back-end Development/Week 8 - Databases/mysqlserver.py
+++ Back-end Development/Week 8 - Databases/mysqlserver.py
@@ -3,12 +3,14 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "tking"
+    password = "tking",
+    database = "alx_exercise"
 
 )
+
 mycursor = mydb.cursor()
+
 mycursor.execute("SHOW DATABASES")
 
 for i in mycursor:
     print(i)
-
