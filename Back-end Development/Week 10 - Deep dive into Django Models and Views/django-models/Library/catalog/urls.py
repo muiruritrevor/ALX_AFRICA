09663bbbde597272a0286df20d3728bd0ea6c0a7
muiruritrevor <trevormuirurii@gmail.com>
2024-10-01 21:46:09 +0300

    added api token view url

--- Back-end Development/Week 10 - Deep dive into Django Models and Views/django-models/Library/catalog/urls.py
+++ Back-end Development/Week 10 - Deep dive into Django Models and Views/django-models/Library/catalog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 from accounts.views import login_view, register, profile, logout_view
 
@@ -10,17 +11,20 @@ router.register(r'transactions', views.TransactionViewSet, basename='transaction
 
 urlpatterns = [
     # Django template-based URLs
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.index_view, name='index'),  # Home page view
     path('books/add/', views.add_book_view, name='add_book_view'),
     path('books/edit/<int:book_id>/', views.edit_book_view, name='edit_book_view'),
     path('books/delete/<int:book_id>/', views.delete_book_view, name='delete_book_view'),
     path('books/<int:book_id>/', views.book_detail_view, name='book_detail_view'),
-    path('books/checkout/<int:book_id>/', views.check_out_book_view, name='check_out_book_view'),
-    path('check-out/<int:book_id>/', views.check_out_book_view, name='check_out_book_view'),  # Check out a book
-    path('return/<int:transaction_id>/', views.return_book_view, name='return_book_view'), # Return a book
-    path('transactions/', views.user_transactions_view, name='user_transactions'),   # Transaction history
+    path('books/checkout/<int:book_id>/', views.check_out_book_view, name='check_out_book_view'),  # Check out a book
+    path('books/return/<int:transaction_id>/', views.return_book_view, name='return_book_view'),  # Return a book
+    path('transactions/', views.user_transactions_view, name='user_transactions_view'),  # Transaction history
+    path('reading-history/', views.reading_history, name='reading_history'),
+    path('recommendations/', views.recommendations, name='recommendations'),
 
     # DRF API URLs
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/', include(router.urls)),  # This will include the registered viewsets
     path('api/books/', views.api_book_list, name='api_book_list'),
     path('api/books/<int:pk>/', views.api_book_detail, name='api_book_detail'),
